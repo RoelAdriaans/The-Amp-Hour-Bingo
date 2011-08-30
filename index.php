d67a5894addc1dc08ded2248474f3056a7936746
Roel Adriaans <roel@road-support.nl>
2011-08-30 20:03:56 +0200

    Proof of concept import.
    Proof of concept. The thing works!
    It's alive, ALIVE!

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title>The Amp Hour Bingo</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+ <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+        $("#bingoTable td").click(function () {
+            $(this).toggleClass("clicked");
+        });
+    });
+    </script>
+    </head>
+<body>
+    <h1><img width="40px" height="40px" src='img/TheAmpHourLogo_40.png'>The Amp Hour Bingo<img  width="40px" height="40px" src='img/TheAmpHourLogo_40.png'></h1>
+<p>Click on the cell to toggle background colour.</p>
+    <table id='bingoTable'>
+        <tr>
+            <td>Dave Says "Bugger Off"</td>
+            <td>Non-Electronic Related Tangent</td>
+            <td>Dave Starts a Rant</td>
+            <td>Discussion of Engineers Pursuing Finance Careers</td>
+            <td>Mighty Ohm Makes a Guest Apperance</td>
+        </tr>
+        <tr>
+            <td>3D Printing Segment</td>
+            <td>Open Source Discussion</td>
+            <td>Host Mentions Their Home Lab Setup</td>
+            <td>Guest Apperance</td>
+            <td>Chris' Baby Face Brought Up</td>
+        </tr>
+
+        <tr>
+            <td>Reference to Dilbert World</td>
+            <td>Technicial Difficulties</td>
+            <td><img width="100px" height="100px" src='img/TheAmpHourLogo_100.png'></td>
+            <td>Dave Shows His Age</td>
+            <td>"I'll Send You a Dollar to Buy a Clue"</td>
+
+        </tr>
+        <tr>
+            <td>Discuss Green Technology </td>
+            <td>A "Listener" Calls In</td>
+            <td>Women in Engineering Discussion</td>
+            <td>Home Made IC Argument</td>
+            <td>Dave Insults the USAv
+        </tr>
+        <tr>
+            <td>Chris Starts a Rant</td>
+            <td>Of Course the CEO of Company X is Listening</td>
+            <td>Hackerspaces Discussed</td>
+            <td>Whole Show Without 555 or Arduino Mentioned</td>
+            <td>Jeri Ellsworth Makes a Guest Apperance</td>
+        </tr>
+    </table>
+</body>
